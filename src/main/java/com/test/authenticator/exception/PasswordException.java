198b2e5996f04354ee7aaed82f2a0864e32fddf7
jawad <jawad.rafiq@starzplayarabia.com>
2024-06-16 01:19:06 +0500

    refactor the folders

---
+++ src/main/java/com/test/authenticator/exception/PasswordException.java
@@ -0,0 +1,11 @@
+package com.test.authenticator.exception;
+
+public class PasswordException extends RuntimeException{
+    public PasswordException() {
+        super("Invalid password");
+    }
+    public PasswordException(String message) {
+        super("Invalid password: " + message);
+    }
+
+}
